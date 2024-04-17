ed4cb8bae967dd361cc517e228fe57e7829fc3d1
Chumakov Aleksey <Alexeyse10@gmail.com>
2024-04-17 16:37:30 +0700

    Added date parsing from the date property with type string (RatesResponseDTO model) to the date property with type date (model RatesModel)

--- ExchangeRates/ExchangeRates/Core/Models/RatesModel.swift
+++ ExchangeRates/ExchangeRates/Core/Models/RatesModel.swift
@@ -2,12 +2,24 @@ import Foundation
 
 struct RatesModel {
     let base: CurrencyId
-    let date: String // DateTime !!! Парсинг !!!
+    let date: Date // DateTime !!! Парсинг !!!
     let rates: [CurrencyId : Double]
 }
 
 extension RatesModel {
     init(response: RatesResponseDTO) {
-        self.init(base: response.base, date: response.date, rates: response.rates)
+        var date = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        dateFormatter.timeZone = TimeZone.autoupdatingCurrent
+        if let result = dateFormatter.date(from: response.date) {
+            date = result
+        } else {
+            print("""
+                  Alert: Failed to get the exchange rate update date.
+                  Exchange rates may not be accurate. Try the request again.
+""")
+        }
+        self.init(base: response.base, date: date, rates: response.rates)
     }
 }
