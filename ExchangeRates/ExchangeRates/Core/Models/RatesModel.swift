49d9e63c06e2815c71842e72e417aa4eeeb7248a
Chumakov Aleksey <Alexeyse10@gmail.com>
2024-04-17 17:24:21 +0700

    added an errorGettingDate property to display it if it occurs in an alert

--- ExchangeRates/ExchangeRates/Core/Models/RatesModel.swift
+++ ExchangeRates/ExchangeRates/Core/Models/RatesModel.swift
@@ -4,22 +4,27 @@ struct RatesModel {
     let base: CurrencyId
     let date: Date // DateTime !!! Парсинг !!!
     let rates: [CurrencyId : Double]
+    var errorGettingDate: String?
 }
 
 extension RatesModel {
     init(response: RatesResponseDTO) {
-        var date = Date()
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd"
-        dateFormatter.timeZone = TimeZone.autoupdatingCurrent
-        if let result = dateFormatter.date(from: response.date) {
-            date = result
-        } else {
-            print("""
-                  Alert: Failed to get the exchange rate update date.
-                  Exchange rates may not be accurate. Try the request again.
-""")
+        func castingDate() -> (Date, String?) {
+            var date = Date()
+            var error: String?
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd"
+            dateFormatter.timeZone = TimeZone.autoupdatingCurrent
+            if let result = dateFormatter.date(from: response.date) {
+                date = result
+            } else {
+                error = """
+                        Alert: Failed to get the exchange rate update date.
+                        Exchange rates may not be accurate. Try the request again.
+                        """
+            }
+            return (date, error)
         }
-        self.init(base: response.base, date: date, rates: response.rates)
+        self.init(base: response.base, date: castingDate().0, rates: response.rates, errorGettingDate: castingDate().1)
     }
 }
